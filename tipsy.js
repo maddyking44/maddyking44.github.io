2e2f6659f4f3d14e9bc311d21c4b92d3c6a579a2
ruthbai <minjoobai@gmail.com>
2018-03-27 18:39:56 +1300

    cant get form data

---
+++ tipsy.js
@@ -0,0 +1,27 @@
+// var express = require('express')
+// var bodyParser = require('body-parser')
+// var server = express()
+// server.use(bodyParser.urlencoded({ extended: false }))
+
+// server.get('/', function (req, res) {
+//     //renders the form
+//         res.sendFile(__dirname + '/form.html')
+//     })
+
+// server.post('/form', function (req, res) {
+//     //post to receive the data from the named-compliment form
+//     var data = req.body 
+//     var name = data.firstname
+//     // // console.log({data})
+//     console.log(data)
+//     res.redirect('/nextPage')
+// })
+
+
+
+function profileObj(){
+    var userOne = document.getElementById("profile")
+    console.log(userOne)
+}
+
+
