65ad2727b68f9d75868175562b66b330db6f6f92
engie15 <e.matene15@gmail.com>
2018-03-28 18:04:06 +1300

    installed handlebars dependencies

--- server.js
+++ server.js
@@ -1,49 +1,64 @@
-// var express = require('express')
-// var bodyParser = require('body-parser')
-// var server = express()
+var express = require('express')
+var bodyParser = require('body-parser')
+var server = express()
+var express = require('express-handlebars')
 // server.use(express.static('public'))
-// server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.urlencoded({ extended: false }))
 
-// server.get('/', function (req, res) {
-//     //renders the form
-//         res.sendFile(__dirname + '/form.html')
-//     })
+server.get('/', function (req, res) {
+    //renders the form
+        res.sendFile(__dirname + '/form.html')
+    })
 
-// server.post('/form', function (req, res) {
-//     //post to receive the data from the named-compliment form
-//     var data = req.body 
-//     var name = data.firstname
-//     // // console.log({data})
-//     console.log(data)
-//     console.log(name)
-//     res.redirect('/nextPage')
-// })
+server.post('/displayPage', function (req, res) {
+    //post to receive the data from the named-compliment form
+    var data = req.body 
+    var name = data.firstname + data.lastname
+    var tag = data.tagline
+    var email = data.email 
+    //need to do something with email
+    var profilepic = data.profilepic
+    // // console.log({data})
+    console.log(data)
+    console.log(name)
+    res.redirect('/displayPage')
+})
 
-var userOne = []
+server.get('/displayPage', function (req, res) {
+    //renders the form
+        res.sendFile(__dirname + '/displayPage')
+    })
 
-function profileObj(){
-    var userOne = {
-        firstname: (document.getElementById("firstname").value),
-        lastname: (document.getElementById("lastname").value),
-        tagline: (document.getElementById("tagline").value),
-        email: (document.getElementById("email").value),
-        profilepic: (document.getElementById("profilepic").value)
-    }
-    console.log(userOne)
-}
 
-var userTwo = {
-    firstname: "Sam",
-    lastname: "Jae",
-    tagline: "fish soup",
-    email: "sam.j@hotmail.com"
-}
+
+
+
+
+// var userOne = []
+
+// function profileObj(){
+//     var userOne = {
+//         firstname: (document.getElementById("firstname").value),
+//         lastname: (document.getElementById("lastname").value),
+//         tagline: (document.getElementById("tagline").value),
+//         email: (document.getElementById("email").value),
+//         profilepic: (document.getElementById("profilepic").value)
+//     }
+//     console.log(userOne)
+// }
+
+// var userTwo = {
+//     firstname: "Sam",
+//     lastname: "Jae",
+//     tagline: "fish soup",
+//     email: "sam.j@hotmail.com"
+// }
 
 // function printDisplay () {
-document.getElementById("displayName").innerHTML(userOne.firstname);
+// document.getElementById("displayName").innerHTML(userOne.firstname);
 // }
 
 // printDisplay()
 
 
-// module.exports = server
+module.exports = server
