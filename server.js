8f8b114fe1f34598b1837eed6c2f6e4788386d22
engie15 <e.matene15@gmail.com>
2018-03-28 18:46:27 +1300

    display page is displaying

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var server = express()
-var express = require('express-handlebars')
-// server.use(express.static('public'))
+var hbs = require('express-handlebars')
+server.use(express.static('public'))
 server.use(bodyParser.urlencoded({ extended: false }))
+var person = {}
 
+//get handlebars working
+var hbsConfig = {
+    extname: 'hbs'
+  }
+server.engine('hbs', hbs(hbsConfig)) 
+server.set('view engine', 'hbs')
+
+//render the form
 server.get('/', function (req, res) {
-    //renders the form
-        res.sendFile(__dirname + '/form.html')
-    })
+   res.sendFile(__dirname + '/form.html')
+})
 
 server.post('/displayPage', function (req, res) {
-    //post to receive the data from the named-compliment form
+//get data from form.html and put each part into variables
     var data = req.body 
-    var name = data.firstname + data.lastname
-    var tag = data.tagline
-    var email = data.email 
+    // var person = {
+    //     name: data.firstname + data.lastname,
+    //     tag: data.tagline,
+    //     email: data.email 
+    // }
+    
     //need to do something with email
     var profilepic = data.profilepic
-    // // console.log({data})
     console.log(data)
-    console.log(name)
+    // console.log(name)
     res.redirect('/displayPage')
 })
 
+//attaches the displayPage.hbs file to the /displayPage url
 server.get('/displayPage', function (req, res) {
-    //renders the form
-        res.sendFile(__dirname + '/displayPage')
-    })
+    res.render('displayPage')
+})
+
+
+
+
+
+
+
 
 
 
